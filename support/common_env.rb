d2aebcc8b242f39168e39dd24f5b51eb973b8663
brandondjango <brandonlockridge@gmail.com>
2020-09-29 20:32:21 -0400

    small changes

--- support/common_env.rb
+++ support/common_env.rb
@@ -13,7 +13,7 @@ include PageObject::PageFactory
 
 #Internal directories
 require_rel('../browser_page_models')
-require_rel('../helpers')
+#require_rel('../helpers')
 
 #def visit_page(page)
 #  @browser.goto page.url_for_page
